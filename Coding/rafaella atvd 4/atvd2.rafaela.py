f0fb4c51944d213b51be2c5e47eb811a032c9a74
higor cabral <104106899+higor9770@users.noreply.github.com>
2022-12-07 10:46:25 -0300

    Delete Coding/rafaella atvd 4 directory

--- Coding/rafaella atvd 4/atvd2.rafaela.py
+++
@@ -1,6 +0,0 @@
-""" Escreva um programa para criar uma função usando as seguintes
-condições:
- Ele deve aceitar o nome e o salário do funcionário e exibir ambos.
- Se o salário estiver ausente na chamada de função, atribua o valor
-padrão 9000 ao salário. """
-
